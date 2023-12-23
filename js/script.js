a98ba9e825bd23eb9f1829c0409079d6c95ecc8a
Guilherme <guilhermeandriel78@gmail.com>
2023-12-23 09:09:24 -0300

    funções toggleMode e descricaoProjeto

--- js/script.js
+++ js/script.js
@@ -1,17 +1,68 @@
-const mode = document.querySelector("#switch button")
-
+const buttonMode = document.querySelector(".switch-button");
+const projetos = document.querySelectorAll(".projeto")
+let isButtonMoved = false;
 
 function toggleMode() {
-    const html = document.documentElement
+    const html = document.documentElement;
 
     if (html.classList.contains("light")) {
-        html.classList.remove("light")
+        html.classList.remove("light");
+        if (isButtonMoved) {
+            buttonMode.style.transform = "translateY(-50%) translateX(0)";
+            isButtonMoved = false;
+        }
     } else {
-        html.classList.add("light")
+        html.classList.add("light");
+        if (!isButtonMoved) {
+            buttonMode.style.transform = "translateY(-50%) translateX(32px)";
+            isButtonMoved = true;
+        }
     }
 }
 
 
-mode.addEventListener("click", () =>{
-    toggleMode()
+function descricaoProjeto(descricao){
+    console.log(descricao)
+    let container = document.createElement("div")
+    container.classList.add("container-janela")
+
+    let janela = document.createElement("div")
+    janela.classList.add("janela")
+
+    let close = document.createElement("button")
+    close.classList.add("closeButton")
+    close.classList.add("fa-solid")
+    close.classList.add("fa-xmark")
+    janela.appendChild(close)
+
+    let paragrafo = document.createElement("p")
+    paragrafo.innerHTML = descricao
+    paragrafo.classList.add("paragrafo-janela")
+    janela.appendChild(paragrafo)
+
+    container.appendChild(janela)
+
+    document.body.appendChild(container);
+
+    let dVoltar = janela.querySelector(".closeButton");
+    dVoltar.addEventListener("click", () => {
+        janela.remove();
+        container.remove();
+    });
+
+    
+  
+}
+
+buttonMode.addEventListener("click", () => {
+    toggleMode();
+});
+
+
+
+projetos.forEach(element =>{
+    element.addEventListener("click", ()=>{
+        let descricao = element.getAttribute("data-description")
+        descricaoProjeto(descricao)
+    })
 })
